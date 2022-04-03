a5201de878bfd0ec1a8b047821128d9fc0f08dbd
DanielBarrientos <danielbarrientos0802@gmail.com>
2022-04-03 17:58:00 -0500

    Server con express

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('index.js')
+
+const express = require('express')
+const app = express()
+const port = 3000;
+
+app.get('/', (req, res)=> {
+  res.send('Hola de sde express')
+})
+
+app.listen(port, () =>{
+  console.log('Server on  port:', port)
+})
