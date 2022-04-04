aec847ac0a397594c56b6a4b0e39f05c6cae7e6f
DanielBarrientos <danielbarrientos0802@gmail.com>
2022-04-03 23:43:12 -0500

    agregando validatorHandler

---
+++ middlewares/validator.handler.js
@@ -0,0 +1,20 @@
+
+const  Boom  = require("@hapi/boom")
+
+
+function validatorHandler(schema, property){
+  
+  return (req, res, next) => {
+    
+    const data = req[property]
+    const { error } = schema.validate(data)
+
+    if(error){
+      next(Boom.badData(error))
+    }
+
+    next()
+  }
+}
+
+module.exports = validatorHandler
